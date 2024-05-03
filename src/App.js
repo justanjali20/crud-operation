3e9c3809a0ea84c75f734a3bd0e01c300cec02c1
Anjali Pandey <justanjali20@gmail.com>
2024-05-03 21:18:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import './App.css'
+
+import React from 'react';
+import './App.css';
 import Home from './Component/Home';
 import Add from './Component/Add';
-import Edit from './Component/Edit'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
+import Edit from './Component/Edit';
+import NotFound from './Component/NotFound'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-    return (
-      <div>
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home/>}  ></Route>
-            <Route path='/create' element={<Add/>} ></Route>
-            <Route path='/Edit' element={<Edit/>} ></Route>
-          </Routes>
-        </Router>
-      </div>
-    )
-  }
-  
-  export default App;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Add />} />
+          <Route path="/edit" element={<Edit />} /> 
+          <Route path="*" element={<NotFound />} /> 
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
